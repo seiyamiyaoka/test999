d92fe4a28de3679d9c28201b9d2aa92822e5e8dc
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-09-25 08:23:58 +0000

    1723

--- config/environment.rb
+++ config/environment.rb
@@ -9,11 +9,9 @@ ActionMailer::Base.smtp_settings =
   {
     :user_name => "app41780031@heroku.com",
     :password => "ucdraimp0462",
-    
     :domain => "heroku.com",
     :address => "smtp.sendgrid.net",
     :port => 587,
     :authentication => :plain,
     :enable_starttls_auto => true
-}
     }
