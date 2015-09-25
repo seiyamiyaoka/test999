9c419be3a0a590fd01dba70bc23d8d4c644ff899
seiyamiyaoka <seiyamiyaoka@gmail.com>
2015-09-25 08:07:07 +0000

    joujou

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,4 +1,6 @@
 class BlogsController < ApplicationController
+  before_filter :authenticate_user!
+  
   def index
     @blogs = Blog.all
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get 'blogs' => 'blogs#index'
   
   #blogの登録
